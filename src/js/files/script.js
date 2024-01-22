05f4cf9b633a0d5d47e3a5fec00b0fa3332c2c3c
HolyRaveOS <kitlaruk1995@gmail.com>
2024-01-22 15:30:12 +0200

    add contacts and footer sections

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,15 @@
 import { isMobile } from './functions.js';
 // Підключення списку активних модулів
 import { flsModules } from './modules.js';
+
+const map = L.map('map').setView([49.2965388, 24.9037301], 9);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const marker = L.marker([49.4120451, 24.594862]).addTo(map);
+L.marker([49.2623612, 24.6072249]).addTo(map);
+L.marker([49.1273925, 24.6937889]).addTo(map);
