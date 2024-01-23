8c2bfa445e905c21236d5e9ec6737eb5f7d2d2d9
HolyRaveOS <kitlaruk1995@gmail.com>
2024-01-23 15:16:13 +0200

    add some features

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,6 +5,9 @@ import { flsModules } from './modules.js';
 
 import JustValidate from 'just-validate';
 
+import Toastify from 'toastify-js';
+import 'toastify-js/src/toastify.css';
+
 const map = L.map('map').setView([49.2965388, 24.9037301], 9);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -18,7 +21,7 @@ L.marker([49.2623612, 24.6072249]).addTo(map);
 L.marker([49.1273925, 24.6937889]).addTo(map);
 
 //===========================================================
-const validator = new JustValidate(document.querySelector('.form-block__form'));
+const validator = new JustValidate(document.getElementById('popup-form'));
 
 validator
   .addField(document.querySelector('#basic_name'), [
@@ -34,9 +37,28 @@ validator
     },
   ]);
 
+const validator1 = new JustValidate(document.getElementById('regular-form'));
+
+validator1
+  .addField(document.querySelector('#basic_name1'), [
+    {
+      rule: 'required',
+      errorMessage: "Введіть ваше ім'я",
+    },
+  ])
+  .addField(document.querySelector('#basic_phone1'), [
+    {
+      rule: 'required',
+      errorMessage: 'Введіть ваш телефон',
+    },
+  ]);
+
 //===========================================================
 
-const form = document.querySelector('.form-block__form');
+const form = document.getElementById('popup-form');
+const form1 = document.getElementById('regular-form');
+
+const page = document.querySelector('.page');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -45,8 +67,16 @@ form.addEventListener('submit', function (e) {
   }
 });
 
+form1.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (validator1.isValid) {
+    sendMessage(form1);
+  }
+});
+
 async function sendMessage(form) {
   const formData = new FormData(form);
+  page.classList.add('_sending');
   if (formData) {
     const url = './files/sendmessage.php';
     const response = await fetch(url, {
@@ -55,9 +85,38 @@ async function sendMessage(form) {
     });
     if (response.ok) {
       form.reset();
-      alert('Форма надіслана');
+      page.classList.remove('_sending');
+      Toastify({
+        text: 'Форма надіслана',
+        duration: 3000,
+        destination: false,
+        close: true,
+        gravity: 'top', // `top` or `bottom`
+        position: 'center', // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: 'linear-gradient(to right, #00b09b, #96c93d)',
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
     } else {
-      alert('Сталася помилка');
+      page.classList.remove('_sending');
+      Toastify({
+        text: 'Сталася помилка',
+        duration: 3000,
+        destination: false,
+        close: true,
+        gravity: 'top', // `top` or `bottom`
+        position: 'center', // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background:
+            'linear-gradient(98.3deg, rgb(0, 0, 0) 10.6%, rgb(255, 0, 0) 97.7%)',
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
     }
   }
 }
+
+//==================================================
